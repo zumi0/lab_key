51fe9a2386a5f1a5b8dfaf973ec6c94887fe38f2
Yuto NAKAIZUMI <yutonakaizumi@YutonoMacBook-Pro.local>
2019-08-09 13:49:09 +0900

    style: delete some comments

--- id_detect.py
+++ id_detect.py
@@ -8,10 +8,6 @@ def CONNECTED(tag):
     service_code = [nfc.tag.tt3.ServiceCode(servc >> 6, servc & 0x3f)]
     # bc_id -> student ID
     bc_id = [nfc.tag.tt3.BlockCode(i) for i in range(3)]
-    #bc_name = [nfc.tag.tt3.BlockCode(3)]
     with open('./sid', mode='w') as f:
         f.write(tag.read_without_encryption(service_code, bc_id)[14:23].decode())
         return False
-
-clf = nfc.ContactlessFrontend('usb')
-clf.connect(rdwr={'on-connect': CONNECTED})
