b01399293e68e82cff05d1bcdfe64759e9c22a93
Yuto NAKAIZUMI <toshin.fish@gmail.com>
2019-08-11 21:51:53 +0900

    style: change by some file fix

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import csv
 import nfc
-import id_detect
-import servo
+import connected
+#import servo
 
-with open("sids.csv", "r") as file_obj:
+with open("ids.csv", "r") as file_obj:
     csv_obj = csv.reader(file_obj)
     id_list = []
     for row in csv_obj:
         id_list.append(row[0])
 
+rdwr_options = {
+    'targets': ['212F' , '424F'],
+    'on-connect': connected.CONNECTED
+}
+
 while True:
     clf = nfc.ContactlessFrontend('usb')
     try:
-        clf.connect(rdwr={'on-connect': id_detect.CONNECTED})
+        clf.connect(rdwr=rdwr_options)
     finally:
         clf.close()
-    f = open('sid', 'r')
+    f = open('id', 'r')
     if f.read() in id_list:
         print("OK")
-        servo.servo()
+        #servo.servo()
     f.close()
